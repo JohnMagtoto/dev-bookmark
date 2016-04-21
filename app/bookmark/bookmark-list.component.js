3a57b6c9badf239942310891929f4aa451334768
JohnMagtoto <johnmagtoto@gmail.com>
2016-04-21 10:19:44 +0800

    Added ng2-bootstrap and ionicons

--- app/bookmark/bookmark-list.component.js
+++ app/bookmark/bookmark-list.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './bookmark.service'], function(exports_1, context_1) {
+System.register(['angular2/core', 'ng2-bootstrap/ng2-bootstrap', './bookmark.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['angular2/core', './bookmark.service'], function(exports_1, con
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, bookmark_service_1;
+    var core_1, ng2_bootstrap_1, bookmark_service_1;
     var BookmarlListComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (ng2_bootstrap_1_1) {
+                ng2_bootstrap_1 = ng2_bootstrap_1_1;
             },
             function (bookmark_service_1_1) {
                 bookmark_service_1 = bookmark_service_1_1;
@@ -32,6 +35,10 @@ System.register(['angular2/core', './bookmark.service'], function(exports_1, con
                     core_1.Component({
                         selector: 'bookmark-list-component',
                         templateUrl: './app/bookmark/bookmark-list.component.html',
+                        directives: [
+                            ng2_bootstrap_1.Alert,
+                            ng2_bootstrap_1.BUTTON_DIRECTIVES
+                        ],
                         providers: [
                             bookmark_service_1.BookmarkService
                         ]
--- app/bookmark/bookmark-list.component.ts
+++ app/bookmark/bookmark-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from 'angular2/core' ;
 
+import { Alert, BUTTON_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 // Service
 import { BookmarkService, Bookmark } from './bookmark.service' ;
 
 @Component({
   selector : 'bookmark-list-component',
   templateUrl : './app/bookmark/bookmark-list.component.html',
+  directives : [
+    Alert,
+    BUTTON_DIRECTIVES
+  ],
   providers : [
     BookmarkService
   ]
@@ -21,7 +26,7 @@ export class BookmarlListComponent implements OnInit {
   }
 
   ngOnInit(){
-    
+
   }
 
 
